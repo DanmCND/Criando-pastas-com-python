ae6e0ee491e1b98fb335ce3363777acbbbe7a1f8
Danilo <digital.danmcnd@gmail.com>
2026-02-13 20:29:08 -0300

    remove one elif

--- Atividade.py
+++ Atividade.py
@@ -6,10 +6,8 @@ import os
 while True:
     nome_pasta = input("Digite o nome da pasta que deseja criar: ").lower()
 #2. Verificar se o nome da pasta é válido (não vazio, sem espaços e sem caracteres inválidos)    
-    if " " in nome_pasta:
-        print("O nome da pasta não pode conter espaços. Por favor, tente novamente.")
-    elif not nome_pasta:
-        print("O nome da pasta não pode ser vazio. Por favor, tente novamente.")  
+    if " " in nome_pasta or not nome_pasta:
+        print("O nome da pasta não pode conter espaços ou estar vazio. Por favor, tente novamente.") 
 #3.Verificar se a pasta já existe        
     elif os.path.exists(nome_pasta):
         print(f"A pasta '{nome_pasta}' já existe! Por favor, escolha outro nome.")      
@@ -18,8 +16,8 @@ while True:
         os.makedirs(nome_pasta, exist_ok=True)
         print(f"O nome da pasta será: {nome_pasta}.\n Pasta criada com sucesso!")     
 #5.Bonus perguntar se deseja criar outra pasta
-        criar_outra = input("Deseja criar outra pasta? (s/n): ").lower()
-        if criar_outra != 's':
+        escolha_usuario = input("Deseja criar outra pasta? (s/n): ").lower()
+        if escolha_usuario != 's':
             print("Encerrando o programa. Até mais!")
             break
  
