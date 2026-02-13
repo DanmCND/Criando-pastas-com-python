92ea3b4373c116d00418fbf61d269c9ec07f1cdb
Danilo <digital.danmcnd@gmail.com>
2026-02-12 22:06:34 -0300

    Remoção do enunciado do arquivo da atividade

--- Atividade.py
+++ Atividade.py
@@ -1,39 +1,5 @@
-'''
-🎯 Caso de Uso Real
-Você foi contratado por uma empresa que organiza documentos por cliente.
-Todos os dias, novos clientes chegam e precisam ter uma pasta criada com o nome deles.
-Para evitar erros e retrabalho, a empresa pediu que você desenvolvesse um pequeno programa em Python que:
-Pergunte o nome da pasta
-Crie a pasta automaticamente
-Informe se a pasta já existe
- 
-📋 O que o programa deve fazer
-[x]Perguntar ao usuário:
-[x]Verificar se a pasta já existe:
-[x]Se não existir, criar a pasta
-[x]Se já existir, mostrar a mensagem:
-[x]Mostrar uma mensagem de sucesso caso a pasta seja criada:
- 
-🧠 Conceitos que deverão ser utilizados
-input()
-if / else
-Biblioteca os
-os.path.exists()
-os.mkdir() ou os.makedirs()
- 
-💡 Comportamento esperado
- 
-[x]Caso 1 – Pasta nova
-Digite o nome da pasta que deseja criar: cliente123 Pasta criada com sucesso! 
- 
-[x]Caso 2 – Pasta já existente
-Digite o nome da pasta que deseja criar: cliente123 Essa pasta já existe! 
- 
-⭐ Desafio Extra (Opcional)
-[x]Não permitir nome vazio
-[x]Perguntar se deseja tentar novamente
-[x]Converter o nome da pasta para minúsculo automaticamente
-'''
+#====Atividade====
+
 import os
 
 #1. Perguntar o nome da pasta ao usuiário
