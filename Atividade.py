e65716bb15f6f3472114d70966d2278316a52378
Danilo <digital.danmcnd@gmail.com>
2026-02-13 20:21:17 -0300

    remove .striip

--- Atividade.py
+++ Atividade.py
@@ -4,7 +4,7 @@ import os
 
 #1. Perguntar o nome da pasta ao usuiário
 while True:
-    nome_pasta = input("Digite o nome da pasta que deseja criar: ").strip().lower()
+    nome_pasta = input("Digite o nome da pasta que deseja criar: ").lower()
 #2. Verificar se o nome da pasta é válido (não vazio, sem espaços e sem caracteres inválidos)    
     if " " in nome_pasta:
         print("O nome da pasta não pode conter espaços. Por favor, tente novamente.")
@@ -18,7 +18,7 @@ while True:
         os.makedirs(nome_pasta, exist_ok=True)
         print(f"O nome da pasta será: {nome_pasta}.\n Pasta criada com sucesso!")     
 #5.Bonus perguntar se deseja criar outra pasta
-        criar_outra = input("Deseja criar outra pasta? (s/n): ").strip().lower()
+        criar_outra = input("Deseja criar outra pasta? (s/n): ").lower()
         if criar_outra != 's':
             print("Encerrando o programa. Até mais!")
             break
